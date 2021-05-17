1b4c7887cd8351b071244088fdcc4c8fe189f63a
maria.lopezgarcia11@gmail.com <55807153+mariaisblue@users.noreply.github.com>
2021-05-17 14:12:33 +0200

    cambio nombre test ot

--- test/SubsistemaGestionOrdenesTrabajo/TestSubsistemaGestionOrdenTrabajo_12.java
+++ test/SubsistemaGestionOrdenesTrabajo/TestSubsistemaGestionOrdenTrabajo_12.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class SubsistemaGestionOrdenTrabajoTest {
+class TestSubsistemaGestionOrdenTrabajo_12 {
 
 	@BeforeEach
 	void setUp() throws Exception {
