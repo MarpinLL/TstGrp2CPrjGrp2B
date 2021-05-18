5c81e017bf5c3cd411a7f34aff46c259f7a5764b
unknown <martinezrodriguezabel@gmail.com>
2021-05-18 13:28:42 +0200

    Equals en Estadistica

--- src/Model/Estadistica.java
+++ src/Model/Estadistica.java
@@ -46,4 +46,40 @@ public class Estadistica {
 		this.distribucion = distribucion;
 	}
 
+	//Equals y HashCode
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((coste == null) ? 0 : coste.hashCode());
+		result = prime * result + ((distribucion == null) ? 0 : distribucion.hashCode());
+		result = prime * result + ((numeroTotal == null) ? 0 : numeroTotal.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estadistica other = (Estadistica) obj;
+		if (coste == null) {
+			if (other.coste != null)
+				return false;
+		} else if (!coste.equals(other.coste))
+			return false;
+		if (numeroTotal == null) {
+			if (other.numeroTotal != null)
+				return false;
+		} else if (!numeroTotal.equals(other.numeroTotal))
+			return false;
+		return true;
+	}
+
+	
+	
+	
 }
