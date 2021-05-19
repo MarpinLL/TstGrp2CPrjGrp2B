1cb9eb01f45881316733e1b454c5f4658291c172
David Macías <you@example.com>
2021-05-19 20:06:55 +0200

    Procesos

---
+++ test/SubsistemaGestionProcesos/TestSubsistemaGestionProcesos_08.java
@@ -0,0 +1,33 @@
+package SubsistemaGestionProcesos;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestSubsistemaGestionProcesos_08 {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testActualizar() {
+		fail("Not yet implemented");
+	}
+
+}
